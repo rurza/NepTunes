596c5a89ab057eca66dc2ef565604eec6db289dd
rurza <rurza@rurzabuk.local>
2015-07-11 16:15:03 +0200

    start working on storyboard version

--- NepTunes/AppDelegate.m
+++ NepTunes/AppDelegate.m
@@ -21,25 +21,28 @@
 @property (weak, nonatomic) NSTimer* callTimer;
 
 @property (strong, nonatomic) MusicScrobbler *musicScrobbler;
-@property (strong) IBOutlet NSMenu *statusMenu;
+@property (strong, nonatomic) IBOutlet NSMenu *statusMenu;
 
 @property (weak, nonatomic) IBOutlet NSMenuItem *loveSongMenuTitle;
 @property (weak, nonatomic) IBOutlet NSMenuItem *profileMenuTitle;
 @property (weak, nonatomic) IBOutlet NSMenuItem *similarArtistMenuTtitle;
+
 @property (weak, nonatomic) IBOutlet NSTextField *loginField;
 @property (weak, nonatomic) IBOutlet NSSecureTextField *passwordField;
 @property (weak, nonatomic) IBOutlet NSButton *loginButton;
 @property (weak, nonatomic) IBOutlet NSWindow *window;
 @property (weak, nonatomic) IBOutlet NSView *accountView;
+@property (weak, nonatomic) IBOutlet NSButton *createAccountButton;
 @property (weak, nonatomic) IBOutlet NSProgressIndicator *indicator;
 
 @property NSTimeInterval scrobbleTime;
-@property int currentViewTag;
-@property BOOL menuItemState;
+//@property int currentViewTag;
+//@property BOOL menuItemState;
 
 
-- (IBAction)switchView:(id)sender;
+//- (IBAction)switchView:(id)sender;
 - (IBAction)loginClicked:(id)sender;
+- (IBAction)createNewLastFmAccountInWebBrowser:(id)sender;
 
 
 
@@ -48,6 +51,7 @@
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    
     DDHotKeyCenter *loveSongHotKey = [DDHotKeyCenter sharedHotKeyCenter];
     [loveSongHotKey registerHotKeyWithKeyCode:0x25 modifierFlags:NSControlKeyMask|NSCommandKeyMask target:self action:@selector(loveSong:) object:nil];
     
@@ -57,12 +61,6 @@
     [icon setTemplate:YES];
     self.statusItem.image = icon;
     self.statusItem.menu = self.statusMenu;
-    
-    //    if (![[NSUserDefaults standardUserDefaults] stringForKey:@"firstLaunch"]) {
-    //        NSLog(@"NepTunes first launch. %@", [[NSUserDefaults standardUserDefaults] stringForKey:@"firstLaunch"]);
-    //        [[NSUserDefaults standardUserDefaults] setObject:@"Launched" forKey:@"firstLaunch"];
-    //    }
-    
     
     
     [[NSDistributedNotificationCenter defaultCenter] addObserver:self
@@ -82,6 +80,27 @@
         });
     }
 }
+
+
+
+-(void)awakeFromNib {
+    self.musicScrobbler = [MusicScrobbler sharedScrobbler];
+    if (self.musicScrobbler.scrobbler.session) {
+        [self hideControls:YES];
+        [self.loginButton setTitle:[NSString stringWithFormat:@"Log out %@", self.musicScrobbler.scrobbler.username]];
+        [self.loginButton setAction:@selector(logout)];
+
+    }
+    [[self window] setContentSize:[self.accountView frame].size];
+    [[[self window] contentView ] addSubview:self.accountView];
+    [[[self window] contentView] setWantsLayer:YES];
+    NSColor *color = [NSColor colorWithSRGBRed:0.2896 green:0.5448 blue:0.9193 alpha:1.0];
+    NSMutableAttributedString *colorTitle = [[NSMutableAttributedString alloc] initWithAttributedString:[self.createAccountButton attributedTitle]];
+    NSRange titleRange = NSMakeRange(0, [colorTitle length]);
+    [colorTitle addAttribute:NSForegroundColorAttributeName value:color range:titleRange];
+    [self.createAccountButton setAttributedTitle:colorTitle];
+}
+
 
 - (void)updateTrackInfo {
     [self changeState];
@@ -144,8 +163,11 @@
 - (IBAction)loginClicked:(id)sender {
     if (!([self.passwordField.stringValue isEqualTo: @""] || [self.loginField.stringValue isEqualTo: @""])) {
         [self.indicator startAnimation:self];
-        self.loginField.hidden = YES;
-        self.passwordField.hidden = YES;
+//        self.loginField.hidden = YES;
+//        self.passwordField.hidden = YES;
+//        [self.createAccountButton setHidden:YES];
+        [self hideControls:YES];
+
         [self.loginButton setTitle:@"Logging in..."];
         [self.loginButton setEnabled:NO];
         [self.musicScrobbler.scrobbler getSessionForUser:self.loginField.stringValue password:self.passwordField.stringValue successHandler:^(NSDictionary *result) {
@@ -154,10 +176,12 @@
             
             [self.loginButton setTitle:[NSString stringWithFormat:@"Log out %@", result[@"name"]]];
             self.loginField.stringValue = @"";
-            self.loginField.hidden = YES;
+//            self.loginField.hidden = YES;
             self.passwordField.stringValue = @"";
-            self.passwordField.hidden = YES;
-            
+//            self.passwordField.hidden = YES;
+//            [self.createAccountButton setHidden:YES];
+            [self hideControls:YES];
+            [self resizePreferences];
             [self.loginButton setEnabled:YES];
             [self changeState];
             [self.loginButton setAction:@selector(logout)];
@@ -168,8 +192,11 @@
             }
             else {
                 [self.indicator stopAnimation:self];
-                self.passwordField.hidden = NO;
-                self.loginField.hidden = NO;
+//                self.passwordField.hidden = NO;
+//                self.loginField.hidden = NO;
+//                [self.createAccountButton setHidden:NO];
+
+                [self hideControls:NO];
                 self.passwordField.stringValue = @"";
                 [self.loginButton setTitle:@"Log in"];
                 [self.loginButton setEnabled:YES];
@@ -191,11 +218,19 @@
 - (void)logout {
     [self.loginButton setTitle:@"Log in"];
     [self.musicScrobbler logOut];
-    self.loginField.hidden = NO;
-    self.passwordField.hidden = NO;
+//    self.loginField.hidden = NO;
+//    self.passwordField.hidden = NO;
+//    [self.createAccountButton setHidden:NO];
+    [self hideControls:NO];
+    [self resizePreferences];
     [self changeState];
     [self.loginButton setAction:@selector(loginClicked:)];
     
+}
+
+- (IBAction)createNewLastFmAccountInWebBrowser:(id)sender {
+    [[NSWorkspace sharedWorkspace] openURL:[NSURL URLWithString:@"https://secure.last.fm/join"]];
+
 }
 
 
@@ -228,6 +263,7 @@
 -(IBAction)openPreferences:(id)sender {
     [NSApp activateIgnoringOtherApps:YES];
     [self.window makeKeyAndOrderFront:self];
+//    [self resizePreferences];
 }
 
 /*----------------------------------------------------------------------------------------------------------*/
@@ -289,75 +325,95 @@
 #pragma mark - preferences
 
 
-
--(void)awakeFromNib {
-    self.musicScrobbler = [MusicScrobbler sharedScrobbler];
-    if (self.musicScrobbler.scrobbler.session) {
-        self.loginField.hidden = YES;
-        self.passwordField.hidden = YES;
-        [self.loginButton setTitle:[NSString stringWithFormat:@"Log out %@", self.musicScrobbler.scrobbler.username]];
-        [self.loginButton setAction:@selector(logout)];
-    }
-    [[self window] setContentSize:[self.accountView frame].size];
-    [[[self window] contentView ] addSubview:self.accountView];
-    [[[self window] contentView] setWantsLayer:YES];
-}
-
 /*----------------------------------------------------------------------------------------------------------*/
 
+-(void)hideControls:(BOOL)enabled
+{
+    self.loginField.hidden = enabled;
+    self.passwordField.hidden = enabled;
+    [self.createAccountButton setHidden:enabled];
+}
+
+-(void)resizePreferences
+{
+    NSWindow *window = self.window; //tworze window
+//    NSView *account = self.accountView;
+    NSRect newFrame;
+    NSPoint buttonOrigin;
+//    NSSize viewSize;
+    
+    if (self.musicScrobbler.scrobbler.session) {
+//        viewSize = NSSizeFromCGSize(CGSizeMake(account.frame.size.width, account.frame.size.height - 70.0f));
+        newFrame = NSRectFromCGRect(CGRectMake(window.frame.origin.x, window.frame.origin.y + 70.0f, window.frame.size.width, window.frame.size.height -70.0f));
+        buttonOrigin = NSPointFromCGPoint(CGPointMake(self.loginButton.frame.origin.x, self.loginButton.frame.origin.y - 10));
+    }
+    
+    else {
+//        viewSize = NSSizeFromCGSize(CGSizeMake(account.frame.size.width, account.frame.size.height + 70.0f));
+        newFrame = NSRectFromCGRect(CGRectMake(window.frame.origin.x, window.frame.origin.y - 70.0f, window.frame.size.width, window.frame.size.height + 70.0f));
+        buttonOrigin = NSPointFromCGPoint(CGPointMake(self.loginButton.frame.origin.x, self.loginButton.frame.origin.y + 10));
+    }
+
+    [NSAnimationContext runAnimationGroup:^(NSAnimationContext *context) {
+        context.duration = 0.25;
+        [[window animator] setFrame:newFrame display:YES];
+        [[[self loginButton] animator] setFrameOrigin:buttonOrigin];
+    } completionHandler:^{
+
+    }];
+}
 
 -(NSRect)newFrameForNewContentView:(NSView *)view {
-    NSWindow *window = self.window;
-    NSRect newFrameRect = [window frameRectForContentRect:[view frame]];
-    NSRect oldFrameRect = [window frame];
-    NSSize newSize = newFrameRect.size;
-    NSSize oldSize = oldFrameRect.size;
+    NSWindow *window = self.window; //tworze window
+    NSRect newFrameRect = [window frameRectForContentRect:[view frame]]; //tworze nowy rect na podstawie wybranego view.frame
+    NSRect oldFrameRect = [window frame]; //pobieram stary rect obecnego view
+    NSSize newSize = newFrameRect.size; //NSSize nowego view
+    NSSize oldSize = oldFrameRect.size; //NSSize starego view
     
-    NSRect frame  = [window frame];
-    frame.size = newSize;
-    frame.origin.y -= (newSize.height - oldSize.height);
+    NSRect frame  = [window frame]; //frame starego okna
+    frame.size = newSize; // ustawiam na nowe
+    frame.origin.y -= (newSize.height - oldSize.height); //zmieniam oryginalny .y
     return frame;
 }
 
--(NSView *)viewForTag:(int)tag {
-    NSView *view = nil;
-    switch (tag) {
-        case 1:
-            view = self.accountView;
-            break;
-        default:
-            view = self.accountView;
-            break;
-    }
-    return view;
-}
-
-
--(BOOL)validateToolbarItem:(NSToolbarItem *)item {
-    if ([item tag] == self.currentViewTag) return NO;
-    else return YES;
-}
-
-
-
--(IBAction)switchView:(id)sender {
-    int tag = (int)[sender tag];
-    NSView *view = [self viewForTag:tag];
-    NSView *previousView = [self viewForTag:self.currentViewTag];
-    self.currentViewTag = tag;
-    
-    NSRect newFrame = [self newFrameForNewContentView:view];
-    [NSAnimationContext beginGrouping];
-    
-    if ([[NSApp currentEvent] modifierFlags] & NSShiftKeyMask) {
-        [[NSAnimationContext currentContext] setDuration:1.0];
-    }
-    [[[[self window] contentView] animator] replaceSubview:previousView with:view];
-    [[[self window] animator] setFrame:newFrame display:YES];
-    
-    [NSAnimationContext endGrouping];
-}
-
+//-(NSView *)viewForTag:(int)tag {
+//    NSView *view = nil;
+//    switch (tag) {
+//        case 1:
+//            view = self.accountView;
+//            break;
+//        default:
+//            view = self.accountView;
+//            break;
+//    }
+//    return view;
+//}
+//
+//
+//-(BOOL)validateToolbarItem:(NSToolbarItem *)item {
+//    if ([item tag] == self.currentViewTag) return NO;
+//    else return YES;
+//}
+//
+//
+//
+//-(IBAction)switchView:(id)sender {
+//    int tag = (int)[sender tag]; //tag nadawcy by rozpoanc co zostalo klikniete
+//    NSView *view = [self viewForTag:tag]; //view kliknietego menu
+//    NSView *previousView = [self viewForTag:self.currentViewTag]; //view obecnego menu
+//    self.currentViewTag = tag; //ustawiam obecny tag na ten klikniety
+//    
+//    NSRect newFrame = [self newFrameForNewContentView:view]; //tworze frame na podstawie wybranego view
+//    [NSAnimationContext beginGrouping];//rozpoczynam animacje
+//    
+//    if ([[NSApp currentEvent] modifierFlags] & NSShiftKeyMask) {
+//        [[NSAnimationContext currentContext] setDuration:1.0]; //jej czas
+//    }
+//    [[[[self window] contentView] animator] replaceSubview:previousView with:view]; //zmieniam view
+//    [[[self window] animator] setFrame:newFrame display:YES]; //ustawiam nowy frame
+//    
+//    [NSAnimationContext endGrouping]; //koncze animacje
+//}
 
 
 
