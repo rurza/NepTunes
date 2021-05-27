504a41aed842ab4d4a534127574fc0bfbd3c17c1
Adam Różyński <adam@micropixels.pl>
2021-05-27 22:24:31 +0200

    🚧 more experiments with plug-ins

--- NepTunes/AppDelegate.swift
+++ NepTunes/AppDelegate.swift
@@ -24,24 +24,24 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         store = Store(initialState: AppState(), reducer: appReducer, environment: AppEnvironment())
         playerObserver = PlayerObserver(store: store)
         // Create the SwiftUI view that provides the window contents.
-        let bundle = Bundle.main
-        let pluginsPath = bundle.builtInPlugInsPath!
-        let statusBarPlugin = Bundle(path: pluginsPath + "/StatusBarInfo.nepext")
-        let principalClass = statusBarPlugin?.principalClass as? PluginsInterface.Type
-        pluginInstance = initPlugin(from: principalClass)
-       
-        Bundle.main.loadAppleScriptObjectiveCScripts()
-        let musicAppleScriptClass: AnyClass = NSClassFromString("MusicScript")!
-        let bridge = musicAppleScriptClass.alloc() as! MusicBridge
+//        let bundle = Bundle.main
+//        let pluginsPath = bundle.builtInPlugInsPath!
+//        let statusBarPlugin = Bundle(path: pluginsPath + "/StatusBarInfo.nepext")
+//        let principalClass = statusBarPlugin?.principalClass as? PluginsInterface.Type
+//        pluginInstance = initPlugin(from: principalClass)
+//
+//        Bundle.main.loadAppleScriptObjectiveCScripts()
+//        let musicAppleScriptClass: AnyClass = NSClassFromString("MusicScript")!
+//        let bridge = musicAppleScriptClass.alloc() as! MusicBridge
 
 
-        note = DistributedNotificationCenter.default().addObserver(forName: NSNotification.Name(rawValue: "com.apple.Music.playerInfo"), object: nil, queue: nil) { note in
-        //    print(note.userInfo as Any?)
-            print(bridge.currentTrack)
-            if let track = bridge.currentTrack {
-                self.pluginInstance?.trackDidChange(track.title, byArtist: track.artist)
-            }
-        }
+//        note = DistributedNotificationCenter.default().addObserver(forName: NSNotification.Name(rawValue: "com.apple.Music.playerInfo"), object: nil, queue: nil) { note in
+//        //    print(note.userInfo as Any?)
+//            print(bridge.currentTrack)
+//            if let track = bridge.currentTrack {
+//                self.pluginInstance?.trackDidChange(track.title, byArtist: track.artist)
+//            }
+//        }
 
         
         let contentView = ContentView()
@@ -67,6 +67,26 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
+    }
+    
+    func application(_ sender: NSApplication, openFile filename: String) -> Bool {
+        let fs = FileManager.default
+        if let path = NSSearchPathForDirectoriesInDomains(.applicationSupportDirectory, .userDomainMask, true).first {
+            let name = (filename as NSString).lastPathComponent
+            let folderPath = path + "/NepTunes/PlugIns"
+            do {
+                try fs.createDirectory(atPath: folderPath, withIntermediateDirectories: true, attributes: nil)
+            } catch {
+                print(error)
+            }
+            let finalPath = folderPath + "/\(name)"
+            do {
+                try fs.moveItem(atPath: filename, toPath: finalPath)
+            } catch {
+                print(error)
+            }
+        }
+        return false
     }
 
 
